2831400477f547e33deac61bb76588f982cd9dea
will.leyton <will.leyton@valid.com>
2018-03-20 16:40:23 -0500

    Modificar FeingClient en MicroserviceTwoClient

--- src/main/java/com/leysoft/app/client/MicroserviceTwoClient.java
+++ src/main/java/com/leysoft/app/client/MicroserviceTwoClient.java
@@ -5,13 +5,16 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+//@FeignClient(name = "microservice-two")
+@FeignClient(name = "api-gateway-server")
 @RibbonClient(name = "microservice-two")
-@FeignClient(name = "microservice-two")
 public interface MicroserviceTwoClient {
 	
-	@GetMapping(value = {"/saludo"})
+	public static final String SERVICE_NAME = "microservice-two";
+	
+	@GetMapping(value = {"/" + SERVICE_NAME + "/saludo"})
 	public Object[] getAll();
 	
-	@GetMapping(value = {"/saludo/{id}"})
+	@GetMapping(value = {"/" + SERVICE_NAME + "/saludo/{id}"})
 	public Object get(@PathVariable("id") Long id);
 }
